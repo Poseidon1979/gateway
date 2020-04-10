c825a06c0beba9935d3377e3f4c0adbe862bd752
Poseidon1979 <paul.yang1979@gmail.com>
2020-04-10 20:21:28 +0800

    first push

---
+++ pushChange.sh
@@ -0,0 +1,6 @@
+git commit src/main/java/cn/yanghaitao/example/gateway/GatewayApplication.java -m "Change pushed"
+git commit src/main/resource/application.yaml -m "Change pushed"
+#git commit src/main/java/cn/yanghaitao/example/gateway/HelloController.java -m "Change pushed"
+git commit /Users/pyang/software/PCF_2.0/Tanzu/gateway/gateway/pom.xml -m "Change pushed"
+#git commit /Users/pyang/software/PCF_2.0/Tanzu/gateway/gateway/yamlGenerator.sh -m "Change pushed"
+git push
